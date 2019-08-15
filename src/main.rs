c9c8ebf78a00d66dcc8cd5ae69ead2d1183b886c
paul wisehart <paul@oldcode.org>
2019-08-15 11:59:46 -0400

    basic askama works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-#[macro_use]
-extern crate tera;
+extern crate askama;
+
+use askama::Template;
+
+#[derive(Template)] // This will generate the code...
+
+#[template(path = "hello.html")] // relative to teamplstes dir in cratre root
+
+struct HelloTemplate<'a> { //name can be anythiung
+	name: &'a str, //firld name shiould mastch vbariable name
+}
 
 fn main() {
+	let hello = HelloTemplate { name: "worldasd" };
+	
 	println!("Content-type: text/html\n\n");
-	println!("<h1>simple cgi</h1>\n");
+	println!("{}", hello.render().unwrap());
 }
+
