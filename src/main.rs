6a7ed39824862b5bcb848e78c8bd6ba8fd5d0d85
paul wisehart <paul@oldcode.org>
2019-08-19 16:09:00 -0400

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use rust_web::http_headers;
 extern crate askama;
 use askama::Template;
@@ -5,13 +7,21 @@ use askama::Template;
 #[derive(Template)]
 #[template(path = "main.html")]
 struct MainPage<'a> {
+	// the mutability of a struct is in its binding (all or nothing)
     name: &'a str,
+	qs: Option<&'a str>,
 }
 
 fn main() {
-    //let hello = HelloTemplate { name: "DSDSDSworldasd" };
-    let main = MainPage { name: "*-name--" };
+	//l = fread(buf, 1, CL, stdin);
+    
+    let mut main_page = MainPage { name: "*-name-#", qs: Some("asd"), };
+	main_page.qs = None; 
+
+	let _qs = "QUERY_STRING";
+	//main_page.qs = env::var(qs).unwrap_or("ERROR".to_string()); 
+	//main_page.qs = env::var(qs);
 
     http_headers();
-    print!("{}", main.render().unwrap());
+    print!("{}", main_page.render().unwrap());
 }
